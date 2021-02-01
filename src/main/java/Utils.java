65824d4195d6fc4fc60de21cc32c38d7c181d6c6
Fredrik Welander <fredrik.welander@subsite.fi>
2021-02-01 23:00:24 +0200

    Fix public

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -3,12 +3,11 @@ public class Utils {
     /**
      * Metod för att beräkna distansen mellan två geografiska koordinater
      */
-    private static double getDistance(double lon1, double lat1, double lon2, double lat2) {
+    public static double getDistance(double lon1, double lat1, double lon2, double lat2) {
         lon1 = lon1 * Math.PI / 180.0;
         lat1 = lat1 * Math.PI / 180.0;
         lon2 = lon2 * Math.PI / 180.0;
         lat2 = lat2 * Math.PI / 180.0;
-
 
         double dlon = lon2 - lon1;
         double dlat = lat2 - lat1;
